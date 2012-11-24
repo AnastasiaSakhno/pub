67e7429e0034071670ce7bb372bb05816a3df79d
AnastasiaSakhno <anastasia.sakhno@gmail.com>
2012-11-24 16:02:47 +0200

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makhnopub::Application.config.secret_token = 'a7bddc0d4496981608a180849d83ac482b6834aabe8da8b4453da9c0bb0c7f217721be90623ad77e387e1efad64ae869cee67caf032e784162a5851ee90f648c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Makhnopub::Application.config.session_store :cookie_store, key: '_makhnopub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Makhnopub::Application.config.session_store :active_record_store
