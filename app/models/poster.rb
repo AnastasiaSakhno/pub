c698c916d06df34e681e80d725a25f4ca187f7a0
AnastasiaSakhno <anastasia.sakhno@gmail.com>
2012-11-25 13:56:00 +0200

    designed main page with bootstrap. just a draft

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -5,4 +5,6 @@ class Poster < ActiveRecord::Base
   validates :title, :presence => true
   validates :context, :presence => true
   validates :approximate_date, :presence => true
+
+  scope :ordered_posters, -> { order(:approximate_date) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Makhnopub::Application.routes.draw do
   resources :posters
+  match 'home' => 'welcome#index'
+  root :to => 'welcome#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
