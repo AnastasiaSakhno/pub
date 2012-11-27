8ffa1654e180eeddd2790b4af5cf696766f61426
AnastasiaSakhno <anastasia.sakhno@gmail.com>
2012-11-27 23:59:13 +0200

    add locales switching

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+    // Setup drop down menu
+    $('.dropdown-toggle').dropdown();
+
+    // Fix input element click problem
+    $('.dropdown input, .dropdown label').click(function(e) {
+        e.stopPropagation();
+    });
+});
