b2b667f8a84a71825324b8fc32602e6f33dd42b2
anastasia <anastasia.sakhno@gmail.com>
2013-04-08 00:28:09 +0300

    refactored design. added products addition after arrival creation

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,6 +1,7 @@
 class Arrival < ActiveRecord::Base
   belongs_to :product
   belongs_to :user, :foreign_key => :receiver_id
+  before_create :add_products
   attr_accessible :count, :incoming_price, :sale_price, :product_id, :receiver_id
 
   validates :product_id, :presence => true
@@ -8,4 +9,14 @@ class Arrival < ActiveRecord::Base
   validates :incoming_price, :presence => true
   validates :sale_price, :presence => true
   validates :count, :presence => true, :numericality => { :greater_than => 0 }
+
+  private
+
+  def add_products
+    product = Product.find(self.product_id)
+    product.total_count += self.count
+    product.incoming_price = self.incoming_price
+    product.sale_price = self.sale_price
+    product.save!
+  end
 end
--- spec/models/arrival_spec.rb
+++ spec/models/arrival_spec.rb
@@ -8,4 +8,26 @@ describe Arrival do
   it { should validate_presence_of(:count) }
   it { should belong_to(:product) }
   it { should belong_to(:user) }
+
+  describe "before create" do
+    it "should change product" do
+      load_seeds
+      user = FactoryGirl.create(:user)
+      product = FactoryGirl.create(:product)
+      count = 20
+      incoming_price = 10
+      sale_price = 30
+      arrival = Arrival.new
+      arrival.product_id = product.id
+      arrival.incoming_price = incoming_price
+      arrival.sale_price = sale_price
+      arrival.count = count
+      arrival.receiver_id = user.id
+      arrival.save!
+      real_product = Product.find(product.id)
+      real_product.total_count.should eq(product.total_count + count)
+      real_product.incoming_price.should eq(incoming_price)
+      real_product.sale_price.should eq(sale_price)
+    end
+  end
 end
