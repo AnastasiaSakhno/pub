81bc925aae8d0d2c9d2aa12e1def0eb675373671
Anastasiia Sakhno <ask@Anastasiias-MacBook-Pro.local>
2019-04-06 20:13:46 +0300

    hours from/to become time columns

--- app/models/table_reservation.rb
+++ app/models/table_reservation.rb
@@ -2,17 +2,20 @@
 # encoding: utf-8
 
 class TableReservation < ActiveRecord::Base
-  attr_accessible :chat_id, :date, :hall, :hour_from, :hour_to, :people_number, :table, :user_name, :user_phone, :status
+  attr_accessible :chat_id, :date, :hall, :time_from, :time_to, :people_number, :table, :user_name, :user_phone, :status
 
   HALL1_TABLES = (1..13).map { |x| x.to_s }
   HALL2_TABLES = (1..23).map { |x| "П#{x}"}
 
   scope :for_date, ->(date) { where(date: date) }
   scope :for_hall, ->(hall) { where(hall: hall) }
-  scope :for_hour_between, ->(hour_from, hour_to) { 
-  	where("hour_from <= :hour_from AND hour_to >= :hour_to
-  	 OR hour_from <= :hour_from AND hour_to <= :hour_to
-  	 OR hour_from >= :hour_from AND hour_to >= :hour_to", { hour_from: hour_from, hour_to: hour_to }) }
+  scope :for_time_between, ->(time_from, time_to) { 
+  	where("time_from <= :time_from AND time_to >= :time_to
+  	 OR time_from <= :time_from AND time_to <= :time_to
+  	 OR time_from >= :time_from AND time_to >= :time_to", { time_from: time_from, time_to: time_to }) }
+  scope :not_rejected, -> { where(arel_table[:status].not_eq(:rejected))}
+
+  before_save :define_time_lasts_up, if: :time_from_changed?
 
   def self.latest(chat_id)
   	TableReservation.find_last_by_chat_id(chat_id)
@@ -23,8 +26,13 @@ class TableReservation < ActiveRecord::Base
   	all_tables = latest.hall == 1 ? HALL1_TABLES : HALL2_TABLES
   	reserved_tables = for_date(latest.date)
   						.for_hall(latest.hall)
-  						.for_hour_between(latest.hour_from, latest.hour_to)
-  						.pluck(ActiveRecord::Base.connection.quote_column_name(:table))
+  						.for_time_between(latest.time_from, latest.time_to)
   	all_tables - reserved_tables
+  end
+
+  private
+
+  def define_time_lasts_up
+    self.time_lasts_up = time_from + 30.minutes
   end
 end
--- db/migrate/20190312170534_create_table_reservations.rb
+++ db/migrate/20190312170534_create_table_reservations.rb
@@ -4,9 +4,10 @@ class CreateTableReservations < ActiveRecord::Migration
       t.integer :chat_id, limit: 5, null: false
       t.date :date, null: false
       t.integer :hall
-      t.integer :table
-      t.integer :hour_from
-      t.integer :hour_to
+      t.string :table
+      t.time :time_from
+      t.time :time_lasts_up
+      t.time :time_to
       t.string :user_name
       t.string :user_phone
       t.integer :people_number
